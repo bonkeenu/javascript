e2aa1246bea51e971a256fd455cf2110a537c209
nao sakano <bonkeenu@gmail.com>
2017-10-21 23:40:44 +0000

    added number validations

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,7 +1,6 @@
-const number = 64
-
 // try to FizzBuzz
-const getFizzBuzazNumbers = OutputFizzBuzz(number);
+const number = -321;
+OutputFizzBuzz(number);
 
 /**
  * This function judge the input number and display "Fizz" or "Buzz" or "FizzBuzz" or simply number.  
@@ -9,18 +8,47 @@ const getFizzBuzazNumbers = OutputFizzBuzz(number);
  */
 function OutputFizzBuzz(inputNumber) {
 
-    for (inputNumber; inputNumber <= 100; inputNumber++) {
-        if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
-            console.log("FizzBuzz");
-        }
-        else if (inputNumber % 3 === 0) {
-            console.log("Fizz");
-        }
-        else if (inputNumber % 5 === 0) {
-            console.log("Buzz");
-        }
-        else {
-            console.log('Number is,' + inputNumber);
+    // string to int
+    inputNumber = Number(inputNumber);
+    // validate input number
+    const isCorrectNum = checkCorrectNumber(inputNumber);
+    if (isCorrectNum) {
+        for (inputNumber; inputNumber <= 100; inputNumber++) {
+
+            if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+                console.log("FizzBuzz");
+            }
+            else if (inputNumber % 3 === 0) {
+                console.log("Fizz");
+            }
+            else if (inputNumber % 5 === 0) {
+                console.log("Buzz");
+            }
+            else {
+                console.log('Number is： ' + inputNumber);
+            }
         }
     }
+}
+
+/**
+ * find the inport number of uncorrect 
+ * @param {number}
+ * @return {boolean}
+ */
+function checkCorrectNumber(checkNum) {
+    // imput number can not has flot
+    if (Math.round(checkNum) !== checkNum) {
+        console.log('Error ： Please imput a number of integer');
+
+        return false;
+    }
+
+    // input number can only has 1 to 100 number of integer
+    if (0 > checkNum || checkNum > 100) {
+        console.log('Error ： Please input number of 1~100 and integer');
+
+        return false;
+    }
+    return true;
 }
