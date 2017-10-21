39bfbc853abf69b984bc7a49c6a5bdd338bc5720
nao sakano <bonkeenu@gmail.com>
2017-10-21 22:20:24 +0000

    try to creating the FizzBuzz Problem

--- fizzbuzz/index.js
+++ fizzbuzz/index.js
@@ -1,1 +1,26 @@
-console.log("hoge");
+const number = 64
+
+// try to FizzBuzz
+const getFizzBuzazNumbers = OutputFizzBuzz(number);
+
+/**
+ * This function judge the input number and display "Fizz" or "Buzz" or "FizzBuzz" or simply number.  
+ * @param inputNumber {number}
+ */
+function OutputFizzBuzz(inputNumber) {
+
+    for (inputNumber; inputNumber <= 100; inputNumber++) {
+        if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
+            console.log("FizzBuzz");
+        }
+        else if (inputNumber % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (inputNumber % 5 === 0) {
+            console.log("Buzz");
+        }
+        else {
+            console.log('Number is,' + inputNumber);
+        }
+    }
+}
